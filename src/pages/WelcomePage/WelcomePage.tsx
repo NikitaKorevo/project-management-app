2f54e603c7e637345fba8ff358c5b89ea9666a73
LehaGer <leha.ger97@mail.ru>
2022-05-22 23:53:34 +0300

    feat: created initial common info about project, team, task

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -14,6 +14,24 @@ const WelcomePage: React.FC = () => {
       <div className="DELETE_BLOCK">
         <button onClick={() => dispatch(setLanguage('ru'))}>change language in state</button>
         <p>{language}</p>
+        <div>
+          <div>
+            Welcome to: Project-management-app
+            <div>
+              A project management system - is an application that helps an individual in a team or
+              group of developers achieve their goals.
+            </div>
+          </div>
+          <div>
+            <div>Developed by team:</div>
+            <div>
+              <div>Nikita Korevo</div>
+              <div>Alex Mz</div>
+              <div>Alexey Gerasimchuk</div>
+            </div>
+          </div>
+          <div>final task of REACT 2022Q1 course</div>
+        </div>
       </div>
     </div>
   );
