031be9a45d47f09c175ee730f06fcf2c23c7769b
LehaGer <leha.ger97@mail.ru>
2022-05-30 01:04:39 +0300

    feat: added possibility to dragging columns

--- src/components/Board/BoardColumn/BoardColumn.tsx
+++ src/components/Board/BoardColumn/BoardColumn.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import React, { FC, useState } from 'react';
 import {
   Box,
   Button,
@@ -17,6 +17,9 @@ import { columnAPI } from '../../../services/columnAPI';
 import ConfirmationModal from '../../ConfirmationModal/ConfirmationModal';
 import BoardTaskCreationForm from '../BoardTaskCreationForm/BoardTaskCreationForm';
 import BoardTask from '../BoardTask/BoardTask';
+import styles from './BoardColumn.module.css';
+import { setColumnDragState, setTaskDragState } from '../../../store/reducers/draggingSlice';
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
 
 interface IBoardColumnProps {
   boardId: string;
@@ -29,6 +32,9 @@ const BoardColumn: FC<IBoardColumnProps> = ({ boardId, columnId, title, order })
   const { data: allTasks, isLoading, isError } = taskAPI.useGetAllTasksQuery({ boardId, columnId });
   const [deleteColumn] = columnAPI.useDeleteColumnMutation();
   const [updateColumn] = columnAPI.useUpdateColumnMutation();
+  const [updateTaskPosition] = taskAPI.useUpdateTaskPositionMutation();
+  const { columnDragState, taskDragState } = useAppSelector((state) => state.dragging);
+  const dispatch = useAppDispatch();
 
   const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
   const [isBoardTaskCreationFormOpen, setIsBoardTaskCreationFormOpen] = useState(false);
@@ -72,6 +78,75 @@ const BoardColumn: FC<IBoardColumnProps> = ({ boardId, columnId, title, order })
     return <BoardTask key={id} taskData={task} />;
   });
 
+  const dragOverHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (
+      (e.target as HTMLDivElement).className.includes(styles.boardColumn) &&
+      columnDragState.state
+    ) {
+      (e.target as HTMLDivElement).classList.add(styles.onDragOver);
+    }
+  };
+
+  const dragLeaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    (e.target as HTMLDivElement).classList.remove(styles.onDragOver);
+  };
+
+  const dragStartHandler = (
+    e: React.DragEvent<HTMLDivElement>,
+    boardColumn: string,
+    title: string
+  ) => {
+    e.stopPropagation();
+    if (!taskDragState.state && !columnDragState.state) {
+      dispatch(
+        setColumnDragState({
+          state: true,
+          columnId: boardColumn,
+          title: title,
+        })
+      );
+    }
+  };
+
+  const dragEndHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    (e.target as HTMLDivElement).classList.remove(styles.onDragOver);
+    dispatch(setColumnDragState({ state: false }));
+  };
+
+  const onDropHandler = (
+    e: React.DragEvent<HTMLDivElement>,
+    boardColumn: string,
+    order: number
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
+    (e.target as HTMLDivElement).classList.remove(styles.onDragOver);
+    if (taskDragState.state) {
+      updateTaskPosition({
+        boardId,
+        columnId: taskDragState.columnId ?? '',
+        taskId: taskDragState.taskId ?? '',
+        userId: taskDragState.userId ?? '',
+        title: taskDragState.title ?? '',
+        description: taskDragState.description ?? '',
+        order: allTasks?.length ? Math.max(...allTasks.map((task) => task.order)) + 1 : 1,
+        newColumnId: boardColumn,
+      });
+    }
+    if (columnDragState.state) {
+      updateColumn({
+        boardId,
+        columnId: columnDragState.columnId ?? '',
+        title: columnDragState.title ?? '',
+        order,
+      });
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -83,6 +158,13 @@ const BoardColumn: FC<IBoardColumnProps> = ({ boardId, columnId, title, order })
         backgroundColor: '#fff',
         borderRadius: 2,
       }}
+      draggable={!taskDragState.state}
+      onDragOver={(e) => dragOverHandler(e)}
+      onDragLeave={(e) => dragLeaveHandler(e)}
+      onDragEnter={(e) => dragStartHandler(e, columnId, title)}
+      onDragEnd={(e) => dragEndHandler(e)}
+      onDrop={(e) => onDropHandler(e, columnId, order)}
+      className={`${styles.boardColumn} ${columnDragState.state ? styles.onDragging : ''}`}
     >
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
         {isTitleEditMode ? (
