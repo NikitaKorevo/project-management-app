ba51146c01c27e6ac440041cb2256db6723eaf97
MrAlexMz <86052472+MrAlexMz@users.noreply.github.com>
2022-05-31 02:13:53 +0300

    feat: change app routing based on user authorisation state

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -7,15 +7,21 @@ import BoardPage from '../../pages/BoardPage/BoardPage';
 import SignUpPage from '../../pages/SignUpPage/signUpPage';
 import SignInPage from '../../pages/SignInPage/SignInPage';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+import { useAppSelector } from '../../hooks/redux';
 
 const AppRouter: React.FC = () => {
+  const { isAuth } = useAppSelector((state) => state.basis);
+
   return (
     <Routes>
       <Route path={APP_ROUTES.WELCOME} element={<WelcomePage />} />
-      <Route path={APP_ROUTES.MAIN} element={<MainPage />} />
-      <Route path={`${APP_ROUTES.BOARD}/:boardId`} element={<BoardPage />} />
-      <Route path={APP_ROUTES.SIGN_IN} element={<SignInPage />} />
-      <Route path={APP_ROUTES.SIGN_UP} element={<SignUpPage />} />
+      <Route path={APP_ROUTES.MAIN} element={isAuth ? <MainPage /> : <SignInPage />} />
+      <Route
+        path={`${APP_ROUTES.BOARD}/:boardId`}
+        element={isAuth ? <BoardPage /> : <SignInPage />}
+      />
+      <Route path={APP_ROUTES.SIGN_IN} element={isAuth ? <MainPage /> : <SignInPage />} />
+      <Route path={APP_ROUTES.SIGN_UP} element={isAuth ? <MainPage /> : <SignUpPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
